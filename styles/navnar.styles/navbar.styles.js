57538510dcbd38c1ac9a5018cb9ec7f791978d9c
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-19 14:57:23 +0200

    bloat removed

---
+++ styles/navnar.styles/navbar.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledNav = styled.nav`
+  .section-center {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* background: #def; */
+    padding: 2rem 0;
+  }
+`;
