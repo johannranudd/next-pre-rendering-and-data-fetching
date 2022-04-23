9342b6c6b240708c743492ba548889dceb8a0870
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-23 18:47:30 +0200

    HEAD component in layout. styling

--- pages/success/index.jsx
+++ pages/success/index.jsx
@@ -1,14 +1,29 @@
-import Button from '../../components/ui/Button';
+import styled from 'styled-components';
 import Link from 'next/link';
 
 const SuccessPage = () => {
   return (
-    <div>
+    <StyledDiv>
       <h1>Success!</h1>
-      <p>thank you for your puchase</p>
-      <Link href={`/`}>Continue shopping</Link>
-    </div>
+      <div>
+        <p>Thank you for your purchase</p>
+        <Link href={`/`}>Continue shopping</Link>
+      </div>
+    </StyledDiv>
   );
 };
 
 export default SuccessPage;
+
+const StyledDiv = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -100%);
+  div {
+    text-align: center;
+    a {
+      text-decoration: underline;
+    }
+  }
+`;
