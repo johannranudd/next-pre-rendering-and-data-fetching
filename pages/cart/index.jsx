fb68102416e92163d34308503f5ec556ad48590c
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-28 11:43:53 +0200

    typo

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -105,7 +105,7 @@ const Cart = () => {
         <form onSubmit={handleSubmit}>
           <label>Fake card number (4 digits)</label>
           <p className='inputAlert'>
-            {inputAlert && 'Fake creadit card number must be four digits'}
+            {inputAlert && 'Fake credit card number must be four digits'}
           </p>
           <div className='input-and-button'>
             <input
